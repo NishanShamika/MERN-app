d451f1ee62cc21727ef28278e9ba479d38c02c3f
Nishan Shyamika <nishan@smbapps.com.au>
2023-01-16 02:36:21 +0530

    Initialization project and create userName module

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
-function App() {
+import Username from './components/Username';
+import Password from './components/Password';
+import Recovery from './components/Recovery';
+import PageNotFound from './components/PageNotFound';
+import Reset from './components/Reset';
+import Register from './components/Register';
+
+/**root routers */
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Username>Root Route</Username>,
+  },
+  {
+    path: '/register',
+    element: <Register>Register Route</Register>,
+  },
+  {
+    path: '/password',
+    element: <Password>Register Route</Password>,
+  },
+  {
+    path: '/recovery',
+    element: <Recovery>Register Route</Recovery>,
+  },
+  {
+    path: '/reset',
+    element: <Reset>Register Route</Reset>,
+  },
+  {
+    path: '*',
+    element: <PageNotFound>Register Route</PageNotFound>,
+  },
+]);
+
+export default function App() {
   return (
-    <div className="App">
-      <h1>Do it</h1>
-    </div>
+    <main>
+      <RouterProvider router={router}></RouterProvider>
+    </main>
   );
 }
-
-export default App;
